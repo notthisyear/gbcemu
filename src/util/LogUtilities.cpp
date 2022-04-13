ec82747fc42ae7656998ffd58d934cb6382979b0
Calle Lindquist <lindquist.calle@gmail.com>
2022-04-13 18:46:17 +0200

    Add Cartridge logger

--- src/util/LogUtilities.cpp
+++ src/util/LogUtilities.cpp
@@ -3,26 +3,24 @@
 
 namespace gbcemu {
 
-const std::unordered_map<LoggerType, std::string>
-    LogUtilities::m_logger_names = { { LoggerType::Internal, "internal" },
-                                     { LoggerType::Cpu, "cpu" },
-                                     { LoggerType::Mmu, "mmu" } };
+const std::unordered_map<LoggerType, std::string> LogUtilities::m_logger_names = {
+    { LoggerType::Internal, "internal" },
+    { LoggerType::Cpu, "cpu" },
+    { LoggerType::Mmu, "mmu" },
+    { LoggerType::Cartridge, "cartridge" },
+};
 
-std::unordered_map<LoggerType, std::shared_ptr<spdlog::logger>> &
-LogUtilities::loggers() {
-    static std::unordered_map<LoggerType, std::shared_ptr<spdlog::logger>>
-        loggers;
+std::unordered_map<LoggerType, std::shared_ptr<spdlog::logger>> &LogUtilities::loggers() {
+    static std::unordered_map<LoggerType, std::shared_ptr<spdlog::logger>> loggers;
     return loggers;
 }
 
 void LogUtilities::init() { spdlog::set_level(spdlog::level::trace); }
 
-void LogUtilities::log(LoggerType type, LogLevel log_level,
-                       const std::string &log_message) {
+void LogUtilities::log(LoggerType type, LogLevel log_level, const std::string &log_message) {
     auto logger = loggers().find(type);
     if (logger == loggers().end()) {
-        loggers().emplace(
-            type, spdlog::stdout_color_mt(m_logger_names.find(type)->second));
+        loggers().emplace(type, spdlog::stdout_color_mt(m_logger_names.find(type)->second));
         return log(type, log_level, log_message);
     }
 
--- src/util/LogUtilities.h
+++ src/util/LogUtilities.h
@@ -5,19 +5,17 @@
 #include <unordered_map>
 
 namespace gbcemu {
-enum class LoggerType { Internal, Cpu, Mmu };
+enum class LoggerType { Internal, Cpu, Mmu, Cartridge };
 
 enum class LogLevel { Trace, Debug, Info, Warning, Error, Critical };
 
 class LogUtilities {
   public:
-    static void log(LoggerType type, LogLevel log_level,
-                    const std::string &log_message);
+    static void log(LoggerType type, LogLevel log_level, const std::string &log_message);
     static void init();
 
   private:
     static const std::unordered_map<LoggerType, std::string> m_logger_names;
-    static std::unordered_map<LoggerType, std::shared_ptr<spdlog::logger>> &
-    loggers();
+    static std::unordered_map<LoggerType, std::shared_ptr<spdlog::logger>> &loggers();
 };
 }
